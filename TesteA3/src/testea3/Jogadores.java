125c78fd086b1dc2e0ccf7ccab9913a6329b5a41
Vinicius Alves Lordaro <viniciuslordaro@gmail.com>
2023-05-02 20:20:28 -0300

    Atualização Jogo
    Teste A3

---
+++ TesteA3/src/testea3/Jogadores.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testea3;
+
+/**
+ *
+ * @author profslpa
+ */
+public class Jogadores {
+    String nome;
+
+    public Jogadores(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
---
+++ TesteA3/src/testea3/TesteA3.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testea3;
+import java.util.Scanner;
+import java.util.ArrayList;
+/**
+ *
+ * @author profslpa
+ */
+public class TesteA3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static ArrayList<Jogadores> arrayJogadores = new ArrayList<Jogadores>();
+
+    public static void main(String[] args) {
+                Inicio home = new Inicio();
+        home.setVisible(true);
+        home.setLocationRelativeTo(null);
+        home.setSize(400, 300);
+        home.setResizable(false);
+        
+    
+               
+    }
+    
+}
